ebb9c612763912d1bfc46144c899262805ba8868
Matt Russo <matthew.russo91@gmail.com>
2025-10-06 14:54:50 -0400

    add userRoutes.js with GET /user/:id route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,28 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
+import { connectDB } from '../config/db.js';
+
+const db = await connectDB();
 
 // @route         GET /user/:id
 // @description   Get a user's information
 // @access        Private
 router.get('/:id', protect, (req, res) => {
-  res.json('GET request for a user.')
+  const userId = req.params.id;
+
+  const q = "SELECT * FROM users WHERE id = ?";
+
+  db.query(q, [userId], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    } else if(data.length === 0) {
+      res.status(404).json({ Error: "User ID not found" })
+    } else {
+      res.status(200).json(data);
+    }
+  });
 })
 
 // @route         DELETE /user/:id
