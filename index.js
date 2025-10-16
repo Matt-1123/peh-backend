2c056c9fdefe756ee1164dc5aeef94b23af92ccb
Matt Russo <matthew.russo91@gmail.com>
2025-10-16 08:12:03 -0400

    add www domain to allowed origins

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://peh-ui.vercel.app',
   'https://projectearthhealth.com',
+  'www.projectearthhealth.com',
   'https://peh-backend-goo1.onrender.com'
 
 ]
