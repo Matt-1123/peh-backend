12001e20ebb347a1d6482fed8e858e5d443aeac7
Matt Russo <matthew.russo91@gmail.com>
2025-10-28 18:28:34 -0400

    add query logic for DELETE /user/:id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -29,14 +29,40 @@ router.get('/:id', protect, (req, res) => {
 // @description   Delete a user
 // @access        Private
 router.delete('/:id', protect, (req, res) => {
-  res.json('DELETE request for a user.')
+  const userId = req.params.id;
+
+  const q = "SELECT * FROM users where id = ?"
+
+  db.query(q, [userId], (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.json(err);
+    }
+    
+    // Check if user exists
+    if(data.length === 0) {
+      return res.status(404).json({ Error: "User ID not found" })
+    }
+
+    // Check if user matches
+    if(data[0].id !== req.user.id) {
+      return res.status(403).json({ Error: "Not authorized to delete this user." })
+    }
+
+    const delete_q = "DELETE FROM users WHERE id = ?"
+
+    db.query(delete_q, [userId], (err, deleteData) => {
+      if(err) return res.send(err);
+      res.json(deleteData)
+    })
+  })
 })
 
 // @route         PUT /user/:id
 // @description   Delete a user
 // @access        Private
-router.delete('/:id', protect, (req, res) => {
-  res.json('DELETE request for a user.')
+router.put('/:id', protect, (req, res) => {
+  res.json('PUT request for user')
 })
 
 export default router;
